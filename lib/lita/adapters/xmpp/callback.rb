74f9aba7d10c06b9f4753b6683ec6d4271fa31bd
Justin Mazzi <jmazzi@gmail.com>
2013-09-13 11:43:50 -0400

    What

--- lib/lita/adapters/xmpp/callback.rb
+++ lib/lita/adapters/xmpp/callback.rb
@@ -73,7 +73,7 @@ module Lita
           Lita.logger.info("Looking up user with name: #{name}.")
           items = roster.items.detect do |jid, item|
             Lita.logger.info("Checking #{jid}: #{jid_without_domain(jid)} => #{name}.")
-            jid_without_domain(jid) == name || jid_without_domain(jid).sub(/\./, '') == name
+            jid_without_domain(jid) == name || jid_without_domain(jid).sub(/\./, ' ') == name
           end
 
           if items
